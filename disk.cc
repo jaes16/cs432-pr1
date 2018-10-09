f1e4d708fdd3d10b309da35eb783546b9df9c47c
Jae Mahn Surh <20js16@lohani.cs.williams.edu>
2018-10-09 11:57:02 -0400

    hi

--- disk.cc
+++ disk.cc
@@ -24,9 +24,9 @@ int requestNum;
 //vector<int> requests;
 
 // shared state
-int livingRequests;
+int livingRequests = 0;
 vector< tuple<int,int> > waiting;
-
+bool done = false;
 
 
 
@@ -43,66 +43,17 @@ void requester (void *a) {
   int track;
   char s[2048];
 
+  thread_lock(1000);
   // for every track
   while(file >> s){
     track = atoi(s);
 
-    //test
-    //    cout << "here: " << numDisk << ", " << s << endl;
+    thread_lock(1000);
 
-
-    thread_lock(1);
-    bool exist = false;
-    bool contained = true;
-
-    // while the waiting list has another track from this requester, wait
-    while(contained){
-
-      //test
-      //      cout << total << ", " <<  requestNum << ", " << waiting.size() << endl;
-
-      // however, if there are less total tracks than the size of the swaiting list, put in anyway
-      thread_lock(2);
-      if((total <= requestNum) && (waiting.size() < requestNum)){
-	thread_unlock(2);
-	break;
-      }
-      thread_unlock(2);
-
-      //checking if waiting list has another track from this requester
-      for(int i = 0; i < waiting.size(); i++){
-	if(get<0>(waiting[i]) == numDisk){
-	  exist = true;
-	}
-      }
-
-      //waiting list has another track from this requester: wait till servicer services a track, and check again.
-      if(exist){
-	contained = true;	
-
-	thread_wait(1, 0);
-
-	// else exist
-      }else {
-	contained = false;
-      }
-      // for next check.
-      exist = false;
-    }
-   
-    //cout << "req" << c << ": " << s << endl;
-
-    
     //wait till there is a spot in the waiting list
     while(waiting.size() == requestNum){
-      thread_lock(2);
-      if(total < requestNum){
-	thread_unlock(2);
-	break;
-      }
-      thread_unlock(2);
-      //      cout << "here: " << numDisk << endl;
-      thread_wait(1, 0);
+      //      cout << "requester" << numDisk << " waiting" << endl;
+      thread_wait(1000, 0);
     }
 
     
@@ -114,135 +65,96 @@ void requester (void *a) {
 
 
     //wake up servicer and other requesters
-    thread_broadcast(1,1);
-    thread_broadcast(1,0);
-    thread_unlock(1);
-  
+    thread_signal(1000, 1);
+    thread_wait(1000, (numDisk + 100));
+
+
   }
   
 
+  livingRequests--;
+  thread_signal(1000, 1);
 
+  thread_unlock(1000);  
 }
 
 void servicer(void *a) {
   int curSer = -1;
+  int signalTarget = -1;
 
-  // as long as total number of requests can fill the waiting list
-  thread_lock(2);
-  while(total >=  requestNum){
-    thread_unlock(2);
-    thread_lock(1);
-    //    cout << "///////////" << total << ", " << requestNum << endl;
+  thread_lock(1000);
+  while(livingRequests > 0){
 
-    // while waiting list is not full, wait
-    while (waiting.size() < requestNum){
+
+    //test
+    //    cout << livingRequests << endl;
+    //cout << waiting.size() << endl;
+
+
+    
+    while (waiting.size() < min(livingRequests, requestNum)){
       //      cout << "servicer waiting at " << waiting.size() << endl;
-      thread_wait(1,1);
+
+      thread_wait(1000,1);
+
     }
 
+    if(livingRequests == 0){
+      break;
+    }
+    
+
+    
     // execute request
     // first time
     if(curSer == -1) {
+      signalTarget = 0;
       cout << "service requester " << get<0>(waiting[0]) << " track " << get<1>(waiting[0]) << endl;
       curSer = get<1>(waiting[0]);
       waiting.erase(waiting.begin());
-    } else {
+    }
+    else {
       //find nearest to previous track
       int temp = 0;
-      for (int i = 1; i < requestNum; i++){
+      for (int i = 0; i < waiting.size(); i++){
 	if(abs(curSer-get<1>(waiting[i])) < abs(curSer-(get<1>(waiting[temp])))){
 	  temp = i;
 	}
       }
+      signalTarget = get<0>(waiting[temp]);
       //remove this track
       cout << "service requester " << get<0>(waiting[temp]) << " track " << get<1>(waiting[temp]) << endl;
       curSer = get<1>(waiting[temp]);
       waiting.erase(waiting.begin() + temp);
       //cout << "waiting size servicer: " << waiting.size() << endl;
     }
-
-
-    /*    cout<< "//////////////////////" << endl;
-      for(int j = 0; j < waiting.size(); j++){
-	cout << "disk" << get<0>(waiting[j]) << "track " << get<1>(waiting[j]) << endl;
-      }
-      cout<< "//////////////////////" << endl;
-    */
-
+      
+      
     //wake all waiting threads
-    thread_broadcast(1, 0);
-    thread_unlock(1);
-    total--;
-
-  }
-  thread_unlock(2);
-  
-  //  cout << "hi///////////////////////////" << endl;
-
-  for(int i = requestNum-2; i >= 0; i--){
-
-    if(i == 0){
-    cout << "service requester " << get<0>(waiting[0]) << " track " << get<1>(waiting[0]) << endl;
-    break;
-    }
-
     
-    thread_lock(1);
-
-    // execute request
-    //find nearest to previous track
-    int temp = 0;
-    for (int j = 0; j < waiting.size(); j++){
-      if(abs(curSer-get<1>(waiting[j])) < abs(curSer-(get<1>(waiting[temp])))){
-	temp = j;
-      }
-    }
-    //remove this track
-    cout << "service requester " << get<0>(waiting[temp]) << " track " << get<1>(waiting[temp]) << endl;
-    curSer = get<1>(waiting[temp]);
-    waiting.erase(waiting.begin() + temp);
-    //cout << "waiting size servicer: " << waiting.size() << endl;
-
-
-
-    //wake all waiting threads
-    thread_broadcast(1, 0);
-    thread_unlock(1);
-
-    /*    cout<< "//////////////////////" << endl;
-    for(int j = 0; j < waiting.size(); j++){
-      cout << get<0>(waiting[j]) << endl;
-    }
-    cout<< "//////////////////////" << endl; */
+    thread_signal(1000, (signalTarget+100));
+    thread_signal(1000, 0);
+    
   }
 
-  thread_broadcast(5,1);
 
+    thread_unlock(1000);  
+      
 }
+
 
 
 
 
 void initializer(void *a) {
   int argc = (intptr_t)a;
-  int maxRequesters = 0;
   for(int i = 0; i < argc; i++){ //****************made this maxRequesters thing
-    maxRequesters++;
-  }
-  ifstream f;
-  f.open("disk.in0");
-  int count = 0;
-  string s;
-  while(f>>s){
-    count++;
-  }
-  total = count * maxRequesters;
-
-  
+    livingRequests++;
+  }  
   for(int i = 0; i < argc; i++){
     thread_create((thread_startfunc_t) requester, (void *)(intptr_t)i);
   }
-  thread_create((thread_startfunc_t) servicer, (void *) (intptr_t)(count * maxRequesters));
+  thread_create((thread_startfunc_t) servicer, (void *) (intptr_t)a);
 }
 
 
